1523500fbf492a784d8368bf38952741801a24b1
htr999999999 <racyzed@gmail.com>
2020-05-27 21:01:00 -0500

    Proyecto Cineflex creado

---
+++ cineflex12/src/main/java/com/example/cineflext/Cineflex12Application.java
@@ -0,0 +1,13 @@
+package com.example.cineflext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cineflex12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cineflex12Application.class, args);
+	}
+
+}
